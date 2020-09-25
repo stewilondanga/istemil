0dc475d57105493c48a4148c9975132a7425108c
Stewart ilondanga <stilondanga@gmail.com>
2020-09-25 19:02:02 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -155,8 +155,37 @@ submitButton.onclick = () => setNewDrawing(sentence.value);
 textBox.value = "Welcome To Istemil Virtual Global Village";
 setNewDrawing("Welcome To Istemil Virtual Global Village");
 
+/*Chart*/
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ['Offen', 'In Vorbereitung', 'Geschlossen', 'Abgebrochen'],
+    datasets: [{
+      label: 'Status',
+      data: [7, 9, 3, 2],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    legend: {
+      position: 'bottom'
+    }
+  }
+});
 /*Table*/
-
 
 //initialize values
 let elist = document.querySelectorAll(".wood,.stone,.metal,.ammo,.shield");
